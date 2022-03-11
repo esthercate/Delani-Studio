aceff2f3679ad91ef38ac20795654618912fb02c
esthercate <vuthi.esther@gmail.com>
2022-03-11 18:24:42 +0300

    adds jquery code to toggle between what we do section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,40 @@
 $(document).ready(function() {
+  // What we do section
+  $(".imgs1").click(function(){
+    $(".hidden1").toggle();
+    $(".imgs1").toggle()
+  });
+
+  $(".hidden1").click(function(){
+    $(".imgs1").toggle()
+    $(".hidden1").toggle();
+  });
+
+  $(".imgs2").click(function(){
+    $(".hidden2").toggle();
+    $(".imgs2").toggle()
+  });
+
+  $(".hidden2").click(function(){
+    $(".imgs2").toggle()
+    $(".hidden2").toggle();
+  });
+
+  $(".imgs3").click(function(){
+    $(".hidden3").toggle();
+    $(".imgs3").toggle()
+  });
+
+  $(".hidden3").click(function(){
+    $(".imgs3").toggle()
+    $(".hidden3").toggle();
+  });
+
+  //Portfolio Section
+
+
+
+  
 
 
 });
