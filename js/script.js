c3b5b994b0e0bcc74176c202be47669f4d5ee90b
esthercate <vuthi.esther@gmail.com>
2022-03-11 23:16:42 +0300

    completes hover effects on portfolio images

--- js/script.js
+++ js/script.js
@@ -1,40 +1,43 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // What we do section
-  $(".imgs1").click(function(){
+  $(".imgs1").click(function () {
     $(".hidden1").toggle();
     $(".imgs1").toggle()
   });
 
-  $(".hidden1").click(function(){
+  $(".hidden1").click(function () {
     $(".imgs1").toggle()
     $(".hidden1").toggle();
   });
 
-  $(".imgs2").click(function(){
+  $(".imgs2").click(function () {
     $(".hidden2").toggle();
     $(".imgs2").toggle()
   });
 
-  $(".hidden2").click(function(){
+  $(".hidden2").click(function () {
     $(".imgs2").toggle()
     $(".hidden2").toggle();
   });
 
-  $(".imgs3").click(function(){
+  $(".imgs3").click(function () {
     $(".hidden3").toggle();
     $(".imgs3").toggle()
   });
 
-  $(".hidden3").click(function(){
+  $(".hidden3").click(function () {
     $(".imgs3").toggle()
     $(".hidden3").toggle();
   });
 
   //Portfolio Section
 
-
-
-  
-
-
+  $(".card").hover(function () {
+      $(".overlay", this).show();
+      $(".overlay-p", this).show();
+    },
+    function () {
+      $(".overlay", this).hide();
+      $(".overlay-p", this).hide();
+    });
 });
